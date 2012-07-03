54e0cd40b227176c45e9485e1b69fa1e52e4e13f
superacidjax <superacidjax@me.com>
2012-07-03 16:15:36 -0400

    initial commit

---
+++ hippo.rb
@@ -0,0 +1,28 @@
+require 'rubygems'
+require 'sinatra'
+require 'haml'
+require 'sass'
+
+before do
+	headers "Content-Type" => "text/html; charset=utf-8"
+end
+
+helpers do
+
+  def link_to(url,text=url,opts={})
+    attributes = ""
+    opts.each { |key,value| attributes << key.to_s << "=\"" << value << "\" "}
+    "<a href=\"#{url}\" #{attributes}>#{text}</a>"
+  end
+
+end
+
+get '/' do
+	@title = "Hip Hippos!"
+	haml :home
+end
+
+get '/application.css' do
+	headers 'Content-Type' => 'text/css; charset=utf-8'
+	sass :style
+end
